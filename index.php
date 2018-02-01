7b599a68ba33aa45bfcfe4529af3567a2aea0584
akhamsamran <akamsamran1@cnm.edu>
2018-02-01 09:56:51 -0700

    changed html file to php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<!--meta tags-->
+		<meta charset="UTF-8" />
+		<meta name ="viewport" content="width=device=-width, intital-scale=1, shrink-to-fit="no"/>
+
+		<!--link to bootstrap CSS-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<!--LINK TO OTHER CSS FILES(comes after bootstrap CSS)-->
+
+		<!--link to Bootstrap javascript dependencies-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src=""> </script>
+		<script src=""></script>
+
+		<title>Title</title>
+
+	</head>
+
+	<body>
+		<h1></h1>
+
+	</body>
+</html>
