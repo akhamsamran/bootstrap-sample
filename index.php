15d6144728b57644c1788f3146ee5daabce7909e
akhamsamran <akamsamran1@cnm.edu>
2018-02-01 10:04:27 -0700

    added js script src's

--- index.php
+++ index.php
@@ -4,23 +4,28 @@
 
 		<!--meta tags-->
 		<meta charset="UTF-8" />
-		<meta name ="viewport" content="width=device=-width, intital-scale=1, shrink-to-fit="no"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<!--link to bootstrap CSS-->
+		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-		<!--LINK TO OTHER CSS FILES(comes after bootstrap CSS)-->
 
+		<!--LINK TO CUSTOM CSS FILES(comes after bootstrap CSS)-->
+
+		<!--OPTIONAL JAVASCRIPT-->
 		<!--link to Bootstrap javascript dependencies-->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src=""> </script>
-		<script src=""></script>
+		<!--(if it is a heavy site, you can put this block just below the closing body tag)-->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-		<title>Title</title>
+
+		<title>Hello Bootstrap</title>
 
 	</head>
 
 	<body>
-		<h1></h1>
+		<h1>Hello World</h1>
 
 	</body>
 </html>
